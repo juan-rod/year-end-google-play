242ca1ca194c1a2fa970708454dd3074944324f9
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2019-12-05 08:33:49 -0600

    filter and sort complete

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -27,10 +27,9 @@ export default {
   },
   mounted () {
     var now = moment('2017-01-16T00:01:17.414Z').format('YYYY')
-    console.log('moment:', now)
-    this.highestKey()
-  //  this.byYear = this.groupItems('time')
-  //  this.byArtist = this.groupItems('description')
+
+    this.newTopArtistByYear()
+
   },
   computed: {
     gridData () {
@@ -44,46 +43,66 @@ export default {
       })
       return playKeys
     },
-    byYear () {
-      return playData.reduce(function (groups, item) {
-        const val = moment(item['time']).format('YYYY')
-        groups[val] = groups[val] || []
-        groups[val].push(item)
-        return groups
-      }, {})
-    },
-    artistPerYear () {
-      return this.byYear['2017'].reduce(function (groups, item) {
-        const val = item['description']
-        groups[val] = groups[val] || []
-        groups[val].push(item)
-        return groups
-      }, {})
-    },
-    topArtistByYear () {
-      let obj = this.artistPerYear
-      return Object.keys(obj).reduce(function (res, key) {
-        if (res.length < obj[key].length) {
-          res.highestKey = key
-          res.length = obj[key].length
-          res.arr = obj[key]
-        }
-        return res
-      }, { highestKey: null, length: 0, arr: null })
-    }
+    // byYear () {
+    //   return playData.reduce(function (groups, item) {
+    //     const val = moment(item['time']).format('YYYY')
+    //     groups[val] = groups[val] || []
+    //     groups[val].push(item)
+    //     return groups
+    //   }, {})
+    // },
+    // artistPerYear () {
+    //   return this.byYear['2017'].reduce(function (groups, item) {
+    //     const val = item['description']
+    //     groups[val] = groups[val] || []
+    //     groups[val].push(item)
+    //     return groups
+    //   }, {})
+    // },
+    // topArtistByYear () {
+    //   let obj = this.artistPerYear
+    //   return Object.keys(obj).reduce(function (res, key) {
+    //     if (res.length < obj[key].length) {
+    //       res.highestKey = key
+    //       res.length = obj[key].length
+    //       res.arr = obj[key]
+    //     }
+    //     return res
+    //   }, { highestKey: null, length: 0, arr: null })
+    // }
   },
   methods: {
-    highestKey () {
-      let obj = this.artistPerYear
-      let highest = Object.keys(obj).reduce(function (res, key) {
-        if (res.length < obj[key].length) {
-          res.highestKey = key
-          res.length = obj[key].length
-          res.arr = obj[key]
-        }
-        return res
-      }, { highestKey: null, length: 0, arr: null })
-      console.log('highest:', highest)
+    async highestKey () {
+      let obj = await this.artistPerYear
+    },
+    async newTopArtistByYear () {
+      let data = await playData
+      console.log('data', data)
+      // data = data.slice(0,200)
+
+      function groupByYear (acc, data) {
+        const year = moment(data['time']).format('YYYY')
+        acc[year] = (acc[year] || []).concat(data)
+        return acc
+      }
+      const groupBy = keys => array =>
+        array.reduce((objectsByKeyValue, obj) => {
+          const value = keys.map(key => obj[key]).join('-');
+          objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+          return objectsByKeyValue;
+        }, {});
+
+      const getByYear = data.reduce(groupByYear, {})
+      const getArtists = getByYear['2018'].map(data => data.description)
+      const artists = Array.from(new Set(getArtists))
+      const groupByArtist = groupBy(['description'])
+      const getTopArtists = groupByArtist(getByYear['2018'])
+      let result = Object.keys(getTopArtists).map(function(d){
+         return { artist: d, length: getTopArtists[d].length, data:  getTopArtists[d] }
+      })
+      result = result.sort((a,b) => a.length > b.length ? -1 : a.length < b.length ? 1 : 0)
+      console.log('final result', result)
+
     }
   },
   components: { Onboard }
